acd88a9dde5efc778970663ddd86cde29656f5f5
Aaleeyah Ivy <53322518+AaleeyahIvy@users.noreply.github.com>
2022-07-29 13:24:50 -0400

    7292022
    Using Firebase services

---
+++ Users/aivy/Documents/GitHub/YinYang/README.md
@@ -0,0 +1,6 @@
+# Yin-Yang App☯️
+An app that lets couples stay connected while in long distance relationships or out of town.
+<hr>
+Using anime.js for fluid animations and customization.
+COMING SOON!
+
---
+++ Users/aivy/Documents/GitHub/YinYang/display_results.php
@@ -0,0 +1,28 @@
+<?php
+    // get the data from the form
+    $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
+    $name = filter_input(INPUT_POST, 'fullname');
+    $phone = filter_input(INPUT_POST, 'phone');
+ 
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Account Information</title>
+    <link rel="stylesheet" type="text/css" href="main.css"/>
+</head>
+<body>
+    <main>
+        <h1>Everything looks good!</h1>
+        <label>Full Name:</label>
+        <span><?php echo htmlspecialchars($fullname);?></span><br>
+
+        <label>Email Address:</label>
+        <span><?php echo htmlspecialchars($email);?></span><br>
+
+        <label>Phone Number:</label>
+        <span><?php echo htmlspecialchars($phone);?></span><br> 
+    </main>
+</body>
+</html>
+
