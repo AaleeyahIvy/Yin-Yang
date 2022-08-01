c433b4b795af010c6d4c389db94de536f863f1b3
Aaleeyah Ivy <53322518+AaleeyahIvy@users.noreply.github.com>
2022-08-01 18:34:45 -0400

    812022
    save user data

--- Users/aivy/Documents/GitHub/YinYang/display_results.php
+++ Users/aivy/Documents/GitHub/YinYang/display_results.php
@@ -10,7 +10,30 @@
     $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
     $name = filter_input(INPUT_POST, 'fullname');
     $phone = filter_input(INPUT_POST, 'phone');
- 
+
+    $conn = mysqli_connect("localhost", "root", "");
+    if($conn === false){
+        die("ERROR: Could not connect. "
+            . mysqli_connect_error());
+    }
+
+    $name = $_REQUEST['fullname'];
+    $email = $_REQUEST['email'];
+    $phone = $_REQUEST['phone'];
+
+    $sql = "INSERT INTO Users VALUES ('$name', '$email', '$phone')";
+
+    if(mysqli_query($conn, $sql)){
+        echo nl2br("\n$name \n$phone \n $email");
+    } else{
+        echo "ERROR: Hush! Sorry $sql. "
+            . mysqli_error($conn);
+    }
+     
+    // Close connection
+    mysqli_close($conn);
+    ?>
+
 ?>
     <main>
         <h1>Everything looks good!</h1>
@@ -22,6 +45,8 @@
 
         <label>Phone Number:</label>
         <span><?php echo htmlspecialchars($phone);?></span><br> 
+
+        <button><a href="app.html">Go to App</a></button>
     </main>
 </body>
 </html>
