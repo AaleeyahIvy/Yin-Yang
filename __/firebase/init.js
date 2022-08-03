5dbf39b03b24f0517f315db3becb7a34f8792698
Aaleeyah Ivy <53322518+AaleeyahIvy@users.noreply.github.com>
2022-08-03 13:48:23 -0400

    Update init.js

--- __/firebase/init.js
+++ __/firebase/init.js
@@ -1,13 +1,22 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+import { getAuth } from "firebase/auth";
+import firebaseui from "firebaseui";
+
+
 // TODO: Replace the following with your app's Firebase project configuration
 var firebaseConfig = {
-apiKey: "AIzaSyC8pbfAmEmaBFoohuHE1lMXi-KQyhrpqXI",
+    apiKey: "AIzaSyC8pbfAmEmaBFoohuHE1lMXi-KQyhrpqXI",
   authDomain: "yinyang-33041.firebaseapp.com",
   projectId: "yinyang-33041",
   storageBucket: "yinyang-33041.appspot.com",
   messagingSenderId: "1066794609552",
   appId: "1:1066794609552:web:5c0157ed7d32ed273a2115",
-  measurementId: "G-FMBHFQDCKC"
+  measurementId: "G-FMBHFQDCKC",
+  databaseURL: "https://yinyang-33041-default-rtdb.firebaseio.com/"
   };
-  
+  const database = getDatabase(app);
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth(app);
   // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+  firebaseui.initializeApp(firebaseConfig);
