e05adabe6df3009ef8263d20a18c070112a10502
Aaleeyah <samanthastar87@gmail.com>
2022-08-16 13:16:52 -0400

    Update user_verification.php

--- Users/aivy/Documents/GitHub/YinYang/verification/user_verification.php
+++ Users/aivy/Documents/GitHub/YinYang/verification/user_verification.php
@@ -26,7 +26,7 @@
                 <?php echo $activation_error; ?>
             </div>
             <p class="lead">If user account is verified then click on the following button to login.</p>
-            <a class="btn btn-lg btn-success" href="http://localhost:8888/php-user-authentication/index.php"
+            <a class="btn btn-lg btn-success" href="http://localhost/Github/Yin-Yang/Users/aivy/Documents/GitHub/YinYang/signin.php"
                >Click to Login
             </a>
         </div>
