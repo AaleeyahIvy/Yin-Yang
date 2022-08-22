2cef92bc1f84ede5350dc4b52a8b30e1c721977b
Aaleeyah <samanthastar87@gmail.com>
2022-08-22 14:52:55 -0400

    8222022

--- Users/aivy/Documents/GitHub/YinYang/verification/validatesignup.php
+++ Users/aivy/Documents/GitHub/YinYang/verification/validatesignup.php
@@ -92,9 +92,9 @@
                 $body = "Hi,nn This is test email send by PHP Script";
                 $headers = "From: sender\'s email";
                 if(mail($to, $subject, $body, $headers)){
-                    echo "Message sent to" . $_SESSION["email"];
+                    echo "Message sent to " . $email;
               } else {
-                echo "Message not sent to" . $_SESSION["email"];
+                echo "Message not sent to " . $email;
               }
             }
           //}
