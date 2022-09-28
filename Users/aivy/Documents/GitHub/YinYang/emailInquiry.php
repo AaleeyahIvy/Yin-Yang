3bb6f17b076fb16b54f318647acbb7c6f2a79864
Aaleeyah <samanthastar87@gmail.com>
2022-09-28 13:41:55 -0400

    9282022

--- Users/aivy/Documents/GitHub/YinYang/emailInquiry.php
+++ Users/aivy/Documents/GitHub/YinYang/emailInquiry.php
@@ -9,21 +9,19 @@
                 $mail->SMTPDebug = SMTP::DEBUG_SERVER;
                 $mail->Host = 'smtp.gmail.com';
                 $mail->Port = 587;
-                $mail->SMTPSecure = 'tls';
-                $mail->SMTPAuth = true;
-                $mail->Username = 'aaleeyah@yinyangapp.com';
-                $mail->Password = 'dkopxyljgzbkoqlw'; //Set up 2-step authentication and use the app super password given by google admin //remember to hide this when published to github
-                $mail->addReplyTo('austin@yinyangapp.com');
+                $mail->CharSet = PHPMailer::CHARSET_UTF8;
+                $mail->addReplyTo($email);
                 $mail->addAddress($email);
                 $mail->setFrom('aaleeyah@yinyangapp.com', 'Yin Yang Admin/Developer');
                 $mail->Subject = 'YinYang Inquiry';
+                $mail->Body = "Inquiry" . $inquiry;
             if (!$mail->send()) {
                 echo 'Mailer Error: ' . $mail->ErrorInfo;
             } else {
                 echo 'Message was sent to ' . $email;
             }
 ?>
-<!DOCTYPE Html>
+<!DOCTYPE html>
 <html>
     
 </html>
